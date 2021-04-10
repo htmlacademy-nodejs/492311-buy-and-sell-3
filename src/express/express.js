8b84cb2ee99fc72be0a059dffdfabf3f29a316ff
alexandr <lexspb19@gmail.com>
2021-04-10 14:54:01 +0300

    basic routing

---
+++ src/express/express.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require(`express`);
+
+const loginRoutes = require(`./routes/login-routes`);
+const registerRoutes = require(`./routes/register-routes`);
+const searchRoutes = require(`./routes/search-routes`);
+const offersRoutes = require(`./routes/offers-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const mainRoutes = require(`./routes/main-routes`);
+
+const DEFAULT_PORT = 8080;
+
+const app = express();
+
+app.use(`/login`, loginRoutes);
+app.use(`/register`, registerRoutes);
+app.use(`/search`, searchRoutes);
+app.use(`/offers`, offersRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/`, mainRoutes);
+
+app.listen(DEFAULT_PORT);
